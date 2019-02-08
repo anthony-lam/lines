e5a469c6a45204c78792a67c02bf7c6f4eff4401
Anthony <alam6@stuy.edu>
2019-02-08 11:37:53 -0500

    jdfkjs

--- draw.c
+++ draw.c
@@ -8,37 +8,37 @@
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   //octant 1
-  if (x1-x0>0 && y1-y2>0 && x1-x0>y1-y0){
+  if (x1-x0>0 && y1-y0>0 && x1-x0>y1-y0){
     int x = x0;
     int y = y0;
     int a = y1-y0;
     int b = -(x1-x0);
-    int d = 2a+b;
+    int d = 2*a+b;
     while(x<=x1){
       plot(s,c,x,y);
       if (d>0){
 	y++;
-	d+=2b;
+	d+=2*b;
       }
       x++;
-      d+=2a;
+      d+=2*a;
     }
   }
   //octant 2
-  if (x1-x0>0 && y1-y2>0 && x1-x0<y1-y0){
+  if (x1-x0>0 && y1-y0>0 && x1-x0<y1-y0){
     int x = x0;
     int y = y0;
     int a = y1-y0;
     int b = -(x1-x0);
-    int d = a+2b;
+    int d = a+2*b;
     while(y<=y1){
       plot(s,c,x,y);
       if (d<0){
 	x++;
-	d+=2a;
+	d+=2*a;
       }
       y++;
-      d+=2b;
+      d+=2*b;
     }
   }
 }
