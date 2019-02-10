6fd0a294a21e10184cd52c8061be1bdd3c7a44bb
AnthonyLam <alam6@stuy.edu>
2019-02-10 14:51:38 -0500

    .

--- draw.c
+++ draw.c
@@ -7,38 +7,103 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+	int temp;
+	if (x1<x0){
+		temp = x1;
+		x1 = x0;
+		x0 = temp;
+		temp = y1;
+		y1 = y0;
+		y0 = temp;
+	}
+
+	//horizontal
+	if (y0 ==y1){
+		int x = x0;
+		while (x1 > x){
+			plot(s,c,x,y0);
+			x++;
+		}
+		return ;
+	}
+	//vertical
+	if (x0 ==x1){
+		int y = y0;
+		while (y1 > y){
+			plot(s,c,x0,y);
+			y++;
+		}
+		return ;
+	}
   //octant 1
-  if (x1-x0>0 && y1-y0>0 && x1-x0>y1-y0){
-    int x = x0;
-    int y = y0;
-    int a = y1-y0;
-    int b = -(x1-x0);
-    int d = 2*a+b;
-    while(x<=x1){
-      plot(s,c,x,y);
-      if (d>0){
-	y++;
-	d+=2*b;
-      }
-      x++;
-      d+=2*a;
-    }
-  }
+	if (x1-x0>=0 && y1-y0>=0 && x1-x0>= y1-y0){
+		int x = x0;
+		int y = y0;
+		int a = y1-y0;
+		int b = -(x1-x0);
+		int d = 2*a+b;
+		while(x<=x1){
+			plot(s,c,x,y);
+			if (d>0){
+				y++;
+				d+=2*b;
+			}
+			x++;
+			d+=2*a;
+		}
+		return;
+	}
+
   //octant 2
-  if (x1-x0>0 && y1-y0>0 && x1-x0<y1-y0){
-    int x = x0;
-    int y = y0;
-    int a = y1-y0;
-    int b = -(x1-x0);
-    int d = a+2*b;
-    while(y<=y1){
-      plot(s,c,x,y);
-      if (d<0){
-	x++;
-	d+=2*a;
-      }
-      y++;
-      d+=2*b;
-    }
-  }
+	if (x1-x0>0 && y1-y0>0 && x1-x0<=y1-y0){
+		int x = x0;
+		int y = y0;
+		int a = y1-y0;
+		int b = -(x1-x0);
+		int d = a+2*b;
+		while(y<=y1){
+			plot(s,c,x,y);
+			if (d<0){
+				x++;
+				d+=2*a;
+			}
+			y++;
+			d+=2*b;
+		}
+	}
+  	//octant 7
+	if (x1-x0>0 && y1-y0<0 && x0-x1>y1-y0){
+		int x = x0;
+		int y = y0;
+		int a = y1-y0;
+		int b = -(x1-x0);
+		int d = 2*b-a;
+		while(y>=y1){
+			plot(s,c,x,y);
+			if (d<0){
+				x++;
+				d-=2*a;
+			}
+			y--;
+			d+=2*b;
+		}
+	}
+
+	if (x1-x0>0 && y1-y0<0 && x0-x1<=y1-y0){
+		int x = x0;
+		int y = y0;
+		int a = y1-y0;
+		int b = -(x1-x0);
+		int d = b - 2*a;
+		while(x<=x1){
+			plot(s,c,x,y);
+			if (d>0){
+				y--;
+				d+=2*b;
+			}
+			x++;
+			d-=2*a;
+		}
+	}
+
 }
--- main.c
+++ main.c
@@ -10,14 +10,38 @@ int main() {
 
   screen s;
   color c;
-
-  c.red = 0;
-  c.green = MAX_COLOR;
+  c.red = MAX_COLOR;
+  c.green = 0;
   c.blue = 0;
 
   clear_screen(s);
 
-	draw_line(0,0,20,10,s,c);
+  // for (int x = 25; x < 50; x+=2){
+  // 	for (int y = 50; y>=25 ; y--){
+  // 		draw_line(x*10, 500 - x*10, y*10, 500 - y*10, s,c);
+  // 	}
+  // }
+  // for (int y = 0; y < 25; y++){
+  // 	for (int x = 25; x>=0 ; x--){
+  // 		draw_line(x*10, 500 - y*10, y*10, 500 - x*10, s,c);
+  // 	}
+  // }
+
+  for(int x = 0; x <=50; x++){
+  	c.red = x * 5;
+  	c.blue = x*10%255;
+  	c.green = x*20%255;
+  	draw_line(x*10,500,500 - x*10, 0, s,c);
+  }
+  for(int y = 0; y <=50; y++){
+  	c.red = y * 15;
+  	c.blue = y*15%255;
+  	c.green = y*25%255;
+  	draw_line(500,y*10,0,500 - y*10, s,c);
+  }
+
+
+
   display(s);
   save_extension(s, "lines.png");
 }
